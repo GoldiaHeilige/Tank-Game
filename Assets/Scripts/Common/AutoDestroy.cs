c3bda5cbf08c469044f230363a0bc72335209bff
VU-KHANH\vukha <vukhanhvalam@gmail.com>
2025-06-21 23:23:06 +0700

    Model + Basic control

---
+++ Assets/Scripts/Common/AutoDestroy.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class AutoDestroy : MonoBehaviour
+{
+    public float lifetime = 60f;
+
+    private void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+}
+
+// Thêm cái này sau khi Instantiate ( đặc biệt là ở DropSpawner.cs )
+
+//  AutoDestroy ad = go.GetComponent<AutoDestroy>();
+//  if (ad == null)
+//  {
+//     ad = go.AddComponent<AutoDestroy>();
+//     ad.lifetime = 60f;    //  hoặc đặt theo config
+//  }
